da820feefca96d2651a3dbcc73a3e8bbab902be1
Daniel Barros <daniel.barros@cqumail.com>
2023-09-15 22:57:08 +0800

    feature: Add number stats table.

--- NumberTypeStats/Program.cs
+++ NumberTypeStats/Program.cs
@@ -1,12 +1,27 @@
 ﻿string[] types = {"sbyte", "byte", "short", "ushort", "int", "uint", "long",
     "ulong", "float", "double", "decimal"};
 
+// Generate table header
+string tableHeaderBorder = String.Format("{0,-8} {1,-8} {2,-36} {3,-36}",
+                                         "--------",
+                                         "--------",
+                                         "------------------------------------",
+                                         "------------------------------------");
+string tableHeader = String.Format("{0,-8} {1,-8} {2,-36} {3,-36}", "Type", "Bytes", "Min", "Max");
+
 // Print table header
-string tableHeader = String.Format("{0,-8} {1,-8} {2,-40} {3,-40}", "Type", "Bytes", "Min", "Max");
-
+WriteLine(tableHeaderBorder);
 WriteLine(tableHeader);
-
-decimal typeMax = Decimal.MaxValue;
-WriteLine(typeMax);
-string typeMaxString = typeMax + "";
-WriteLine(typeMaxString.Length);
+WriteLine(tableHeaderBorder);
+WriteLine("{0,-8} {1,-8} {2,36} {3,36}", "sbyte", sizeof(sbyte), sbyte.MinValue, sbyte.MaxValue);
+WriteLine("{0,-8} {1,-8} {2,36} {3,36}", "byte", sizeof(byte), byte.MinValue, byte.MaxValue);
+WriteLine("{0,-8} {1,-8} {2,36} {3,36}", "short", sizeof(short), short.MinValue, short.MaxValue);
+WriteLine("{0,-8} {1,-8} {2,36} {3,36}", "ushort", sizeof(ushort), ushort.MinValue, ushort.MaxValue);
+WriteLine("{0,-8} {1,-8} {2,36} {3,36}", "int", sizeof(int), int.MinValue, int.MaxValue);
+WriteLine("{0,-8} {1,-8} {2,36} {3,36}", "uint", sizeof(uint), uint.MinValue, uint.MaxValue);
+WriteLine("{0,-8} {1,-8} {2,36} {3,36}", "long", sizeof(long), long.MinValue, long.MaxValue);
+WriteLine("{0,-8} {1,-8} {2,36} {3,36}", "ulong", sizeof(ulong), ulong.MinValue, ulong.MaxValue);
+WriteLine("{0,-8} {1,-8} {2,36} {3,36}", "float", sizeof(float), float.MinValue, float.MaxValue);
+WriteLine("{0,-8} {1,-8} {2,36} {3,36}", "double", sizeof(double), double.MinValue, double.MaxValue);
+WriteLine("{0,-8} {1,-8} {2,36} {3,36}", "decimal", sizeof(decimal), decimal.MinValue, decimal.MaxValue);
+WriteLine(tableHeaderBorder);
